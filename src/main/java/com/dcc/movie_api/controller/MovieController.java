7b908643ff921737e050fc0a6bb35671738bf38c
WallochMatt <matthewrwalloch@gmail.com>
2022-11-15 10:29:59 -0600

    feat: Created Controller package and file + Set up a Post method

---
+++ src/main/java/com/dcc/movie_api/controller/MovieController.java
@@ -0,0 +1,21 @@
+package com.dcc.movie_api.controller;
+
+import com.dcc.movie_api.data.Movie;
+import com.dcc.movie_api.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @PostMapping("/addMovie")
+    public Movie addMovie(@RequestBody Movie movie){
+        return movieService.saveMovie(movie);
+    }
+
+}

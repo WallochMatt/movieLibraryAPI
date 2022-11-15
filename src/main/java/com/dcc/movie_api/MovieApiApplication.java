1c29a020f01319f6f2f0b053e0609f6b0e3092b4
WallochMatt <matthewrwalloch@gmail.com>
2022-11-15 09:56:02 -0600

    feat: Initializing

---
+++ src/main/java/com/dcc/movie_api/MovieApiApplication.java
@@ -0,0 +1,13 @@
+package com.dcc.movie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcc/movie_api/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcc.movie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
